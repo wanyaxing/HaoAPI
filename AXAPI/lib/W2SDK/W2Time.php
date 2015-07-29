6348ca0fdb8e879f7c11ebc377da090dfee2022b
wanyaxing <asinw@qq.com>
2015-07-29 17:04:52 +0800

    +指定日期所在月最后一天 +指定日期所在年最后一天 +分割日期区间获得数组 ...

--- AXAPI/lib/W2SDK/W2Time.php
+++ AXAPI/lib/W2SDK/W2Time.php
@@ -48,7 +48,7 @@ class W2Time {
     }
 
     /**
-     * 将时间转化成时间戳
+     * 将字符串转化成时间戳
      * @param  [string | int | DateTime] $p_time [description]
      * @return [int]         时间戳
      */
@@ -59,13 +59,13 @@ class W2Time {
 
     /**
      * 取得两个时间（字符串或时间戳）之间的时间差距（秒）
-     * @param  [string | int] $p_datetime  待比较时间
-     * @param  [string | int] $p_datetime2 被比较时间（默认是当前时间）
+     * @param  [string | int] $p_time  待比较时间
+     * @param  [string | int] $p_time2 被比较时间（默认是当前时间）
      * @return int              时间差（秒） ，负数 说明前者在后者之前。
      */
-    public static function getTimeBetweenDateTime($p_datetime,$p_datetime2=null)
+    public static function getTimeBetweenDateTime($p_time=null,$p_time2=null)
     {
-        return W2Time::getTimestamp($p_datetime) - W2Time::getTimestamp($p_datetime2) ;
+        return W2Time::getTimestamp($p_time) - W2Time::getTimestamp($p_time2) ;
     }
 
     /**
@@ -74,7 +74,7 @@ class W2Time {
      * @param  int|string $p_add      时间戳或 字符串  -1 day  next Thursday
      * @return [type]             [description]
      */
-    public static function getTimeAdded($p_time,$p_add=0)
+    public static function getTimeAdded($p_time=null,$p_add=0)
     {
         if (is_int($p_add))
         {
@@ -83,7 +83,14 @@ class W2Time {
         return W2Time::getTimestamp(W2Time::timetostr($p_time) . ' ' .$p_add);
     }
 
-    public static function getTimesArrayBetweenDateTime($p_time1,$p_time2=null,$p_stepType=null)
+    /**
+     * 取得两个时间点之间的时间数组
+     * @param  [string | int] $p_time1    [description]
+     * @param  [string | int] $p_time2    [description]
+     * @param  string $p_stepType 分隔方案 second minute hour day year
+     * @return array             格式化后的时间字符串数组
+     */
+    public static function getTimesArrayBetweenDateTime($p_time1=null,$p_time2=null,$p_stepType=null)
     {
         $_interval = abs(W2Time::getTimeBetweenDateTime($p_time1,$p_time2));
         if ($p_stepType=null)
@@ -150,55 +157,75 @@ class W2Time {
 
     /**
      * 时间转化成当前时间的对应关系
-     * @param  [type] $p_datetime 待转换时间
+     * @param  [type] $p_time 待转换时间
      * @return [string]           对应关系，如5分钟前
      */
-    public static function getTimeToStringBetweenDateTimeWithToday($p_datetime){
-        $p_datetime = W2Time::getTimestamp($p_datetime);
-        $_interval = W2Time::getTimeBetweenDateTime($p_datetime,time());
+    public static function getTimeToStringBetweenDateTimeWithToday($p_time=null){
+        $p_time = W2Time::getTimestamp($p_time);
+        $_interval = W2Time::getTimeBetweenDateTime($p_time,time());
         $_r = '未知';
         if($_interval<-60*60*24*30*12){
-            $_r = sprintf('%s年前 %s',0 - intval($_interval/60/60/24/30/12),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s年前 %s',0 - intval($_interval/60/60/24/30/12),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval<-60*60*24*30){
-            $_r = sprintf('%s月前 %s',0 - intval($_interval/60/60/24/30),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s月前 %s',0 - intval($_interval/60/60/24/30),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval<-60*60*24*7) {
-            $_r = sprintf('%s周前 %s',0 - intval($_interval/60/60/24/7),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s周前 %s',0 - intval($_interval/60/60/24/7),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval<-60*60*24*2){
-            $_r = sprintf('%s天前 %s',0-intval($_interval/60/60/24),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s天前 %s',0-intval($_interval/60/60/24),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval <- 60*60*24*2){
-            $_r = sprintf('%s天前 %s',0-intval($_interval/60/60/24),W2Time::timetostr($p_datetime,'m-d H:i'));
-        } else if (W2Time::getTimeBetweenDateTime($p_datetime,date("Y-m-d 00:00:00",strtotime("-1 day"))) < 0){
-            $_r = sprintf('前天 %s',W2Time::timetostr($p_datetime,'H:i:s'));
-        } else if (W2Time::getTimeBetweenDateTime($p_datetime,date("Y-m-d 00:00:00",time())) < 0){
-            $_r = sprintf('昨天 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('%s天前 %s',0-intval($_interval/60/60/24),W2Time::timetostr($p_time,'m-d H:i'));
+        } else if (W2Time::getTimeBetweenDateTime($p_time,date("Y-m-d 00:00:00",strtotime("-1 day"))) < 0){
+            $_r = sprintf('前天 %s',W2Time::timetostr($p_time,'H:i:s'));
+        } else if (W2Time::getTimeBetweenDateTime($p_time,date("Y-m-d 00:00:00",time())) < 0){
+            $_r = sprintf('昨天 %s',W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<-60*60*2){
-            $_r = sprintf('今天 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('今天 %s',W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<-60*60){
-            $_r = sprintf('一小时前 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('一小时前 %s',W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<-60){
-            $_r = sprintf('%s分钟%s %s',0-intval($_interval/60),$_isBeforeOrAfter?'前':'内',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('%s分钟%s %s',0-intval($_interval/60),$_isBeforeOrAfter?'前':'内',W2Time::timetostr($p_time,'H:i:s'));
         }else if($_interval<0){
-            $_r = sprintf('刚才 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('刚才 %s',W2Time::timetostr($p_time,'H:i:s'));
         }else if($_interval<60){
-            $_r = sprintf('马上 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('马上 %s',W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<60*60){
-            $_r = sprintf('%s分钟内 %s',intval($_interval/60),W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('%s分钟内 %s',intval($_interval/60),W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<60*60*2){
-            $_r = sprintf('一小时后 %s',W2Time::timetostr($p_datetime,'H:i:s'));
-        } else if (W2Time::getTimeBetweenDateTime($p_datetime,date("Y-m-d 00:00:00",strtotime("+1 day"))) < 0){
-            $_r = sprintf('今天 %s',W2Time::timetostr($p_datetime,'H:i:s'));
-        } else if (W2Time::getTimeBetweenDateTime($p_datetime,date("Y-m-d 00:00:00",strtotime("+2 day"))) < 0){
-            $_r = sprintf('明天 %s',W2Time::timetostr($p_datetime,'H:i:s'));
+            $_r = sprintf('一小时后 %s',W2Time::timetostr($p_time,'H:i:s'));
+        } else if (W2Time::getTimeBetweenDateTime($p_time,date("Y-m-d 00:00:00",strtotime("+1 day"))) < 0){
+            $_r = sprintf('今天 %s',W2Time::timetostr($p_time,'H:i:s'));
+        } else if (W2Time::getTimeBetweenDateTime($p_time,date("Y-m-d 00:00:00",strtotime("+2 day"))) < 0){
+            $_r = sprintf('明天 %s',W2Time::timetostr($p_time,'H:i:s'));
         } else if($_interval<60*60*24*7) {
-            $_r = sprintf('%s天后 %s',intval($_interval/60/60/24),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s天后 %s',intval($_interval/60/60/24),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval<60*60*24*30){
-            $_r = sprintf('%s周后 %s',intval($_interval/60/60/24/7),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s周后 %s',intval($_interval/60/60/24/7),W2Time::timetostr($p_time,'m-d H:i'));
         } else if($_interval<60*60*24*30*12){
-            $_r = sprintf('%s月后 %s',intval($_interval/60/60/24/30),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s月后 %s',intval($_interval/60/60/24/30),W2Time::timetostr($p_time,'m-d H:i'));
         } else {
-            $_r = sprintf('%s年后 %s',intval($_interval/60/60/24/30/12),W2Time::timetostr($p_datetime,'m-d H:i'));
+            $_r = sprintf('%s年后 %s',intval($_interval/60/60/24/30/12),W2Time::timetostr($p_time,'m-d H:i'));
         }
 
         return $_r;
+    }
+
+    /**
+     * 获得指定时间所在当月的最后一天的日期 Y-m-d
+     * @param  [type] $p_time [description]
+     * @return string             格式 Y-m-d
+     */
+    public static function getLastDayInSameMonth($p_time=null)
+    {
+        return W2Time::timetostr((W2Time::getTimeAdded(W2Time::timetostr($p_time,'Y-m-1'),'+1 month') - 1),'Y-m-d');
+    }
+
+    /**
+     * 获得指定时间所在当年的最后一天的日期 Y-m-d
+     * @param  [type] $p_time [description]
+     * @return string             格式 Y-m-d
+     */
+    public static function getLastDayInSameYear($p_time=null)
+    {
+        return W2Time::getLastDayInSameMonth(W2Time::timetostr($p_time,'Y-12-1'));
     }
 }
