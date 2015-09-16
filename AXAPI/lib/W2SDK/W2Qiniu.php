0e93e7a87f1d43648470f51c4d8d650c6f366197
wanyaxing <asinw@qq.com>
2015-09-16 19:03:12 +0800

    +	 * 获得七牛格式的压缩图地址

--- AXAPI/lib/W2SDK/W2Qiniu.php
+++ AXAPI/lib/W2SDK/W2Qiniu.php
@@ -205,6 +205,59 @@ class W2Qiniu {
 	}
 
 	/**
+	 * 获得七牛格式的压缩图地址
+	 * @param  string $baseUrl 地址
+	 * @param  int    $width   宽度
+	 * @param  int    $height  高度
+	 * @param  int    $mode    模式	说明
+		* /0/w/<LongEdge>/h/<ShortEdge>	限定缩略图的长边最多为<LongEdge>，短边最多为<ShortEdge>，进行等比缩放，不裁剪。如果只指定 w 参数则表示限定长边（短边自适应），只指定 h 参数则表示限定短边（长边自适应）。
+		* /1/w/<Width>/h/<Height>	    限定缩略图的宽最少为<Width>，高最少为<Height>，进行等比缩放，居中裁剪。转后的缩略图通常恰好是 <Width>x<Height> 的大小（有一个边缩放的时候会因为超出矩形框而被裁剪掉多余部分）。如果只指定 w 参数或只指定 h 参数，代表限定为长宽相等的正方图。
+		* /2/w/<Width>/h/<Height>	    限定缩略图的宽最多为<Width>，高最多为<Height>，进行等比缩放，不裁剪。如果只指定 w 参数则表示限定宽（长自适应），只指定 h 参数则表示限定长（宽自适应）。它和模式0类似，区别只是限定宽和高，不是限定长边和短边。从应用场景来说，模式0适合移动设备上做缩略图，模式2适合PC上做缩略图。
+		* /3/w/<Width>/h/<Height>	    限定缩略图的宽最少为<Width>，高最少为<Height>，进行等比缩放，不裁剪。如果只指定 w 参数或只指定 h 参数，代表长宽限定为同样的值。你可以理解为模式1是模式3的结果再做居中裁剪得到的。
+		* /4/w/<LongEdge>/h/<ShortEdge>	限定缩略图的长边最少为<LongEdge>，短边最少为<ShortEdge>，进行等比缩放，不裁剪。如果只指定 w 参数或只指定 h 参数，表示长边短边限定为同样的值。这个模式很适合在手持设备做图片的全屏查看（把这里的长边短边分别设为手机屏幕的分辨率即可），生成的图片尺寸刚好充满整个屏幕（某一个边可能会超出屏幕）。
+		* /5/w/<LongEdge>/h/<ShortEdge>	限定缩略图的长边最少为<LongEdge>，短边最少为<ShortEdge>，进行等比缩放，居中裁剪。如果只指定 w 参数或只指定 h 参数，表示长边短边限定为同样的值。同上模式4，但超出限定的矩形部分会被裁剪。
+	 * @param  int    $quality 图片质量，取值范围是[1, 100]。默认85，会根据原图质量算出一个修正值，取修正值和指定值中的小值。 注：1. 如果图片的quality值本身大于90，会根据指定quality值进行处理，此时修正值会失效。2. quality后面可以增加 ! ，表示强制使用指定值（eg：100!）3. 支持图片类型：jpg。
+	 * @param  string $format  取值范围：jpg，gif，png，webp等，缺省为原图格式。
+	 * @param  int    $interlace  是否支持渐进显示 * 取值范围：1 支持渐进显示，0不支持渐进显示(缺省为0) * 适用目标格式：jpg * 效果：网速慢时，图片显示由模糊到清晰。
+	 * @return [type]          [description]
+	 */
+	public static function getThumbImageUrl($baseUrl=null,$width=null,$height=null,$mode=0,$quality=null,$format=null,$interlace=null)
+	{
+		if (!is_null($baseUrl))
+		{
+	    	$ops = array($mode);
+
+	    	if (!empty($width)) {
+	    		$ops[] = 'w/' . $width;
+	    	}
+	    	if (!empty($height)) {
+	    		$ops[] = 'h/' . $height;
+	    	}
+	    	if (!empty($quality)) {
+	    		$ops[] = 'q/' . $quality;
+	    	}
+	    	if (!empty($format)) {
+	    		$ops[] = 'format/' . $format;
+	    	}
+
+	    	if (count($ops)>0)
+	    	{
+		    	$thumbString = '?imageView2/' . implode('/', $ops);
+
+		    	if (strpos($baseUrl,'?')!==false)
+		    	{
+		    		$baseUrl = str_replace('?',$thumbString.'&',$baseUrl);
+		    	}
+		    	else
+		    	{
+			    	$baseUrl = $baseUrl . $thumbString ;
+		    	}
+	    	}
+		}
+    	return $baseUrl;
+	}
+
+	/**
 	 * 上传文件到七牛，并获得其预览地址
 	 * @param  string $filePath 本地文件路径
 	 * @param  string $key      存储目标文件名（默认为 md5_filesize.type
