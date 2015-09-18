2ff20c628973f83e705da39e037038a3558b2d22
wanyaxing <asinw@qq.com>
2015-09-18 18:11:26 +0800

    * 增强 get method下的参数拼接

--- AXAPI/lib/W2SDK/W2Web.php
+++ AXAPI/lib/W2SDK/W2Web.php
@@ -34,15 +34,23 @@ class W2Web {
                     curl_setopt($_curl, CURLOPT_POSTFIELDS, $p_postData);
                 }
             }
-            else if (is_array($p_postData))
+            else
             {
-                $p_url .= strpos($p_url,'?')===false?'?':'&';
-                $_params = array();
-                foreach ($p_postData as $key => $value) {
-                        $_v = ($value===true)?'1':(($value===false)?'0':rawurlencode($value));
-                        array_push($_params, sprintf('%s=%s', $key, $_v));
+                if (!is_null($p_postData))
+                {
+                    $_params = $p_postData;
+                    if (is_array($p_postData))
+                    {
+                        $_params = array();
+                        foreach ($p_postData as $key => $value) {
+                                $_v = ($value===true)?'1':(($value===false)?'0':rawurlencode($value));
+                                array_push($_params, sprintf('%s=%s', $key, $_v));
+                        }
+                        $_params .= implode('&',$_params);
+                    }
+                    $p_url .= strpos($p_url,'?')===false?'?':'&';
+                    $p_url .= $_params;
                 }
-                $p_url .= implode('&',$_params);
             }
         }
         curl_setopt($_curl, CURLOPT_URL,            $p_url);
@@ -78,82 +86,25 @@ class W2Web {
             );
         }
         curl_close($_curl);
+
+        if (defined('IS_AX_DEBUG'))
+        {
+            print("\n");var_export($p_url);
+            print("\n");var_export($p_header);
+            print("\n");var_export($p_postData);
+            print("\n");var_export($_result);
+        }
+
         return $_result;
     }
-
-    /**
-     * 发送http请求, 返回结果
-     * @param string url
-     * @param array
-     * <pre>
-     * array(
-     *    'header'         => array(),
-     *    'method'         => 'post/get',
-     *    'post-data'      => array( 'param1'=>'param1', 'file2'=>'@file2.jpg' ),
-     *    'timeout'        => 5,
-     *    'result'         => 'all/header/body(default)'
-     * );
-     * </pre>
-     * @return array|string 根据设置参数里的result返回结果, 默认是body
-     */
-    /*
-    public static function loadStringByUrlWithArrayOptions($p_url, $p_options=null){
-        $curl = curl_init();
-        $opts = array(
-            CURLOPT_URL             => $p_url,
-            CURLOPT_FOLLOWLOCATION  => 1,
-            CURLOPT_TIMEOUT         => 60,
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_NOBODY          => 0,
-            CURLOPT_HEADER          => 0
-        );
-        if(isset($p_options)){
-            if(array_key_exists('header', $p_options)){
-                $opts[CURLOPT_HTTPHEADER] = $p_options['header'];
-            }
-            if(array_key_exists('method', $p_options) && strcasecmp($p_options['method'], 'post')==0){
-                $opts[CURLOPT_POST] = 1;
-                if(array_key_exists('post-data', $p_options)){
-                    $opts[CURLOPT_POSTFIELDS] = $p_options['post-data'];
-                }
-            }
-            if(array_key_exists('timeout', $p_options)){
-                $opts[CURLOPT_TIMEOUT] = $p_options['timeout'];
-            }
-            if(array_key_exists('result', $p_options)){
-                if($p_options['result']=='all'){
-                    $opts[CURLOPT_HEADER] = 1;
-                } else if($p_options['result']=='header'){
-                    $opts[CURLOPT_NOBODY] = 1;
-                    $opts[CURLOPT_HEADER] = 1;
-                }
-            }
-        }
-
-        curl_setopt_array($curl, $opts);
-        $resp = curl_exec($curl);
-        $result = $resp;
-
-        if($p_options!=null && array_key_exists('result', $p_options) && $p_options['result']=='all'){
-            $info = curl_getinfo($curl);
-            $headerSize = $info['header_size'];
-            $result = array(
-                'header'=>trim(substr($resp,0,$headerSize)),
-                'body'=>trim(substr($resp,$headerSize))
-            );
-        }
-        curl_close($curl);
-        return $result;
-    }
-    */
 
     /**
      * 发送http请求, 返回Json数组
      * @param string url
      * @return array Json 数组
      */
-    public static function loadJsonByUrl($p_url){
-        $_c = W2Web::loadStringByUrl($p_url);
+    public static function loadJsonByUrl($p_url, $p_method='get', $p_postData=null, $p_header=null, $p_timeout=30, $p_result='body'){
+        $_c = W2Web::loadStringByUrl($p_url, $p_method, $p_postData, $p_header, $p_timeout, $p_result);
         return isset($_c)?json_decode($_c, true):$_c;
     }
 
@@ -237,17 +188,3 @@ class W2Web {
         return (array_key_exists($p_key, $_COOKIE))?$_COOKIE[$p_key]:null;
     }
 }
-
-
-/**
- * unit test
- */
-/*
-if(array_key_exists('argv', $GLOBALS) && realpath($argv[0]) == __file__){
-    var_dump(W2Web::loadStringByUrl('http://api.appjk.com/1.php', 'post', 'a=1,2,3,4,5', array("client:appjk-web-server")));
-}
-*/
-
-
-
-?>
