a2e6f85845d65bcef677aea70ec9c476f2cc8b1f
wanyaxing <asinw@qq.com>
2015-09-20 17:15:45 +0800

    +支持捕捉致命错误、拼写错误与警告

--- AXAPI/webroot/index.php
+++ AXAPI/webroot/index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors',1);            //错误信息
 ini_set('display_startup_errors',1);    //php启动错误信息
+
 error_reporting(-1);                    //打印出所有的 错误信息
 
 date_default_timezone_set('PRC');//设定时区
@@ -18,6 +19,44 @@ date_default_timezone_set('PRC');//设定时区
     //加载基础方法
     require_once(AXAPI_ROOT_PATH.'/components/Utility.php');
 
+    /**
+     * 主要用于捕捉致命错误，每次页面处理完之后执行检查
+     * @return [type] [description]
+     */
+    function catch_fatal_error()
+    {
+      // Getting Last Error
+       $last_error =  error_get_last();
+
+        // Check if Last error is of type FATAL
+        if(isset($last_error['type']))
+        {
+            // Fatal Error Occurs
+            // Do whatever you want for FATAL Errors
+            $errorMsg = null;
+            switch ($last_error['type']) {
+                case E_ERROR:
+                    $errorMsg = '严重错误：服务器此时无法处理您的请求，请稍后或联系管理员。';
+                    break;
+                case E_PARSE:
+                    $errorMsg = '代码拼写错误：是Peter干的吗，请向管理员举报Peter。';
+                    break;
+                case E_WARNING:
+                    $errorMsg = '警告：出现不严谨的代码逻辑，请告知管理员这个问题。';
+                    break;
+            }
+
+            if (!is_null($errorMsg))
+            {
+                @ob_end_clean();//要清空缓冲区， 从而删除PHPs " 致命的错误" 消息。
+                $results = Utility::getArrayForResults(RUNTIME_CODE_ERROR_UNKNOWN,$errorMsg,null,array('errorContent'=>'Error on line '.$last_error['line'].' in '.$last_error['file'].': '.$last_error['message'].''));
+                echo json_encode($results, JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+        }
+
+    }
+    register_shutdown_function('catch_fatal_error');
 
     //接口格式校验
     $results = Utility::getAuthForApiRequest();
@@ -70,28 +109,31 @@ date_default_timezone_set('PRC');//设定时区
     }
 
     //打印接口返回的数据
-    if (is_array($results) && array_key_exists('errorCode',$results))
+    if (is_array($results))
     {
-        $data = $results['results'];
-        if (is_object($results['results']) && is_subclass_of($results['results'],'AbstractModel'))
+        if (array_key_exists('errorCode',$results))
         {
-            $data = $results['results']->properties();
-        }
-        else if (is_array($results['results']) && array_key_exists(0, $results['results']))
-        {
-            $data = array();
-            foreach ($results['results'] as $_key => $_value) {
-                if (is_object($_value) && is_subclass_of($_value,'AbstractModel'))
-                {
-                    $data[$_key] = $_value->properties();
-                }
-                else
-                {
-                    $data[$_key] = $_value;
+            $data = $results['results'];
+            if (is_object($results['results']) && is_subclass_of($results['results'],'AbstractModel'))
+            {
+                $data = $results['results']->properties();
+            }
+            else if (is_array($results['results']) && array_key_exists(0, $results['results']))
+            {
+                $data = array();
+                foreach ($results['results'] as $_key => $_value) {
+                    if (is_object($_value) && is_subclass_of($_value,'AbstractModel'))
+                    {
+                        $data[$_key] = $_value->properties();
+                    }
+                    else
+                    {
+                        $data[$_key] = $_value;
+                    }
                 }
             }
+            $results['results'] = $data;
         }
-        $results['results'] = $data;
         header('Content-Type:text/javascript; charset=utf-8');
         if (defined('IS_AX_DEBUG'))
         {
@@ -106,6 +148,11 @@ date_default_timezone_set('PRC');//设定时区
     else if (is_string($results))
     {
         echo $results;
+        exit;
+    }
+    else
+    {
+        echo json_encode($results, JSON_UNESCAPED_UNICODE);
         exit;
     }
 
