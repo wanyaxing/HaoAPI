ca4d58d0050c5614e9b7ced93cdeae6775985b20
wanyaxing <asinw@qq.com>
2015-10-14 18:06:35 +0800

    *

--- AXAPI/webroot/apitest/apitest-config.Cache.js
+++ AXAPI/webroot/apitest/apitest-config.Cache.js
@@ -2,7 +2,7 @@
 apiList[apiList.length] = {
         'title':'缓存:清空所有缓存（限管理员）'
         ,'desc':''
-        ,'time':'2015-09-24 15:25:46'
+        ,'time':''
         ,'action':'/cache/emptyCache'
         ,'method':'post'
         ,'request':[
