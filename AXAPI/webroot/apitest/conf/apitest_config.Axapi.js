f2185850edd5ea03306284ac1dd401185f2a51d9
wanyaxing <asinw@qq.com>
2015-12-31 16:18:12 +0800

    *

--- AXAPI/webroot/apitest/conf/apitest_config.Axapi.js
+++ AXAPI/webroot/apitest/conf/apitest_config.Axapi.js
@@ -36,7 +36,7 @@ apiList.push({
      { "key":"page"                  ,"type":"int"        ,"required": true ,"test-value":"1"                        ,"title":"分页，第一页为1，第二页为2，最后一页为-1" ,"desc":"" }
     ,{ "key":"size"                  ,"type":"int"        ,"required": true ,"test-value":"10"                       ,"title":"分页大小" ,"desc":"" }
     ,{ "key":"type"                 ,"type":"string"      ,"required":true ,"test-value":"error"                         ,"title":"日志类型" ,"desc":"限以下值（access: 访问日志, error:错误日志）" }
-    ,{ "key":"datetime"             ,"type":"datetime"      ,"required":false ,"test-value":"error"                         ,"title":"指定日志所在日期（默认当日）" ,"desc":"" }
+    ,{ "key":"datetime"             ,"type":"datetime"      ,"required":false ,"test-value":""                         ,"title":"指定日志所在日期（默认当日）" ,"desc":"" }
   ]
 });
 
