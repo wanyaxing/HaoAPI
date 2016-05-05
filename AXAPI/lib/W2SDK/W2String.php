d0448323e7c44a24120c9015a9572a5864478a32
wanyaxing <wyx@wanyaxing.com>
2016-05-05 14:19:14 +0800

    + 新增 W2String::rc4

--- AXAPI/lib/W2SDK/W2String.php
+++ AXAPI/lib/W2SDK/W2String.php
@@ -406,4 +406,47 @@ class W2String {
         $str = str_replace('ID','Id',$str);
         return strtolower(ltrim(preg_replace_callback('/[A-Z]/', function ($mathes) { return '_' . strtolower($mathes[0]); }, $str), '_'));
     }
+
+    /*
+     * rc4加密算法
+     * $pwd 密钥
+     * $data 要加密的数据
+     */
+    function rc4 ($pwd, $data)//$pwd密钥　$data需加密字符串
+    {
+        $key[] = "";
+        $box[] = "";
+
+        $pwd_length  = strlen($pwd);
+        $data_length = strlen($data);
+
+        for ($i = 0; $i < 256; $i++)
+        {
+            $key[$i] = ord($pwd[$i % $pwd_length]);
+            $box[$i] = $i;
+        }
+
+        for ($j = $i = 0; $i < 256; $i++)
+        {
+            $j       = ($j + $box[$i] + $key[$i]) % 256;
+            $tmp     = $box[$i];
+            $box[$i] = $box[$j];
+            $box[$j] = $tmp;
+        }
+
+        for ($a = $j = $i = 0; $i < $data_length; $i++)
+        {
+            $a       = ($a + 1) % 256;
+            $j       = ($j + $box[$a]) % 256;
+
+            $tmp     = $box[$a];
+            $box[$a] = $box[$j];
+            $box[$j] = $tmp;
+
+            $k       = $box[(($box[$a] + $box[$j]) % 256)];
+            $cipher .= chr(ord($data[$i]) ^ $k);
+        }
+
+        return $cipher;
+    }
 }
