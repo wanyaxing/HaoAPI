4ddb25df0808473a72d1e45242efb870e7679f9a
wanyaxing <wyx@wanyaxing.com>
2016-05-11 10:16:06 +0800

    *

--- AXAPI/lib/W2SDK/W2Mail.php
+++ AXAPI/lib/W2SDK/W2Mail.php
@@ -11,6 +11,9 @@ class W2Mail {
 
     const REQUIRE_PATH = '../PHPMailer/class.phpmailer.php';
 
+    public static $SENDCLOUD_API_USER        = null;
+    public static $SENDCLOUD_API_KEY     = null;
+
     /**
      * 检查依赖项是否存在
      * @return true|false
@@ -29,16 +32,16 @@ class W2Mail {
 
     /**
      * 发送邮件，使用gmail帐户，返回发送结果
-     * @param string $p_toMail 收件人地址
-     * @param string $p_subject 主题
-     * @param string $p_content 正文
-     * @param array  $p_attachment 附件
-     * @param string $p_senderName 发件人名称
-     * @param string $p_senderMail 发件人地址，必须为gmail账户
-     * @param string $p_senderPassword 发件人密码
+     * @param string $pToMail 收件人地址
+     * @param string $pSubject 主题
+     * @param string $pContent 正文
+     * @param array  $pAttachment 附件
+     * @param string $pSenderName 发件人名称
+     * @param string $pSenderMail 发件人地址，必须为gmail账户
+     * @param string $pSenderPassword 发件人密码
      * @return array 结果
      */
-    public static function sendMailWithGmail($p_toMail, $p_subject, $p_content, $p_attachment=null, $p_senderName, $p_senderMail, $p_senderPassword) {
+    public static function sendMailWithGmail($pToMail, $pSubject, $pContent, $pAttachment=null, $pSenderName, $pSenderMail, $pSenderPassword) {
         $_r = array();
         if(!W2Mail::checkRequire()){
             $_r['result'] = 3;
@@ -56,15 +59,15 @@ class W2Mail {
                 $mail->SMTPAuth   = true;                  // enable SMTP authentication
                 $mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
                 $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
-                $mail->Username   = $p_senderMail;          // GMAIL username
-                $mail->Password   = $p_senderPassword;            // GMAIL password
-                $mail->AddReplyTo($p_senderMail, $p_senderName);
-                $mail->AddAddress($p_toMail);
-                $mail->SetFrom($p_senderMail, $p_senderName);
-                $mail->Subject = $p_subject;
-                $mail->MsgHTML($p_content);
-                if (is_array($p_attachment)) {
-                    foreach ($p_attachment as $_a) {
+                $mail->Username   = $pSenderMail;          // GMAIL username
+                $mail->Password   = $pSenderPassword;            // GMAIL password
+                $mail->AddReplyTo($pSenderMail, $pSenderName);
+                $mail->AddAddress($pToMail);
+                $mail->SetFrom($pSenderMail, $pSenderName);
+                $mail->Subject = $pSubject;
+                $mail->MsgHTML($pContent);
+                if (is_array($pAttachment)) {
+                    foreach ($pAttachment as $_a) {
                         if (file_exists($_a)) {
                             $mail->AddAttachment($_a);
                         }
@@ -83,16 +86,43 @@ class W2Mail {
         }
         return $_r;
     }
+
+    public static function sendCloudWithTemple($pToMail, $pSubject, $pSub, $pAttachment=null, $pFromMail, $pFromname = null)
+    {
+        $url = 'http://sendcloud.sohu.com/webapi/mail.send_template.json';
+
+        $substitution_vars = array();
+        $substitution_vars['to'] = W2String::getStringsArray($pToMail);
+        $substitution_vars['sub'] = $pSub;//"sub":{"%name%": ["Ben", "Joe"],"%money%":[288, 497]}
+
+        $params = array();
+        $params['api_user']             = static::$SENDCLOUD_API_USER;//string   是   API_USER
+        $params['api_key']              = static::$SENDCLOUD_API_KEY;//string   是   API_KEY
+        $params['from']                 = $pFromMail;//string   是   发件人地址. from 和发信域名, 会影响是否显示代发
+        $params['substitution_vars']    = $substitution_vars;//string   *   模板替换变量. 在 use_maillist=false 时使用, 如: {"to": ["ben@ifaxin.com", "joe@ifaxin.com"],"sub":{"%name%": ["Ben", "Joe"],"%money%":[288, 497]}}
+        // $params['to']                   = '';//string   *   收件人的地址列表. 在 use_maillist=true 时使用
+        $params['subject']              = $pSubject;//string   否   邮件标题
+        $params['template_invoke_name'] = $pTemplate;//string   是   邮件模板调用名称
+        $params['fromname']             = $pFromname;//string   否   发件人名称. 显示如: ifaxin客服支持 <support@ifaxin.com>
+        // $params['replyto']              = '';//string   否   默认的回复邮件地址. 如果 replyto 没有或者为空, 则默认的回复邮件地址为 from
+        // $params['label']                = '';//int  否   本次发送所使用的标签ID. 此标签需要事先创建
+        // $params['headers']              = '';//string   否   邮件头部信息. JSON 格式, 比如:{"header1": "value1", "header2": "value2"}
+        $params['files']                = $pAttachment;//string   否   邮件附件. 发送附件时, 必须使用 multipart/form-data 进行 post 提交 (表单提交)
+        // $params['resp_email_id']        = '';//string (true, false) 否   是否返回 emailId. 有多个收件人时, 会返回 emailId 的列表
+        // $params['use_maillist']         = '';//string (true, false) 否   参数 to 是否支持地址列表, 默认为 false. 比如: to=users@maillist.sendcloud.org
+        // $params['gzip_compress']        = '';//string (true, false) 否   邮件内容是否使用gzip压缩. 默认不使用 gzip 压缩正文
+
+        $result = W2Web::loadStringByUrl($url,'post',$params);
+
+        return $result;
+    }
 }
 
-/*
-if (isset($argv) && is_array($argv) && realpath($argv[0])==__file__) {
-    $_r = W2Mail::sendMailWithGmail('wantiegang@gmail.com', '邮件主题', '<html><head></head><body>查看列表内容<ul><li>1</li><li>2</li><li>3</li></ul></body></html>',
-        array(),
-        'AppJK Team', 'appjk.team@gmail.com', ''
-    );
-    var_dump($_r);
-}
-*/
 
-?>
+
+//静态类的静态变量的初始化不能使用宏，只能用这样的笨办法了。
+if (W2Mail::$SENDCLOUD_API_USER == null && defined('W2SMS_USER'))
+{
+    W2Mail::$SENDCLOUD_API_USER      = W2MAIL_SENDCLOUD_API_USER;
+    W2Mail::$SENDCLOUD_API_KEY       = W2MAIL_SENDCLOUD_API_KEY;
+}
